db857892b8d4eb0b2c1b9006d055e07bb4555b7f
LiangYu-Xidian <lyu@xidian.edu.cn>
2024-11-04 01:17:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def setup_package():
         version="1.0",
         description='scMFG: a single-cell Multi-omics Integration Method based on Feature Grouping',
         url='https://github.com/LiangYu-Xidian/scMFG',
-        author='Yu',
+        author='Liang Yu',
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',  # 如果README是Markdown格式
         packages=find_packages(),
